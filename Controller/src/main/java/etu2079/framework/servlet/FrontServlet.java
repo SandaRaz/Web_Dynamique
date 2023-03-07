22521bfa4d6f43455f7f361cc8155085872f0a50
SandaRaz <119030837+SandaRaz@users.noreply.github.com>
2023-03-07 10:32:20 +0300

    2303071031

--- Controller/src/main/java/etu2079/framework/servlet/FrontServlet.java
+++ Controller/src/main/java/etu2079/framework/servlet/FrontServlet.java
@@ -1,4 +1,70 @@
 package etu2079.framework.servlet;
 
-public class FrontServlet {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontServlet extends HttpServlet {
+    RequestDispatcher dispat;
+    HttpServletRequest request;
+    HttpServletResponse response;
+
+    public RequestDispatcher getDispat() {
+        return this.dispat;
+    }
+    public void setDispat(RequestDispatcher dispat) {
+        this.dispat = dispat;
+    }
+    public HttpServletRequest getRequest() {
+        return this.request;
+    }
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+    public HttpServletResponse getResponse() {
+        return this.response;
+    }
+    public void setResponse(HttpServletResponse response) {
+        this.response = response;
+    }
+
+    public void redirect(String indexOfFile) throws ServletException, IOException {
+        this.setDispat(this.request.getRequestDispatcher(indexOfFile));
+        this.getDispat().forward(this.getRequest(), this.getResponse());
+    }
+
+    protected void processRequest() throws IOException {
+        PrintWriter out = this.getResponse().getWriter();
+        String incommingURL = String.valueOf(this.getRequest().getRequestURL());
+        String target = this.getTarget(incommingURL);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    private String removeHttpProtocoleStr(String URL){
+        return URL.split("//")[1];
+    }
+
+    private String getTarget(String URL){
+        URL = removeHttpProtocoleStr(URL);
+        if(URL.toLowerCase().contains("_war")){
+            System.out.println("'war' artifact detected");
+            return URL.split("/")[2];
+        }else{
+            return URL.split("/")[1];
+        }
+    }
+
 }
