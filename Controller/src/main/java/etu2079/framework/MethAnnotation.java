cf3db7b2732bb230ef1e337c033f59ebc1a1eaa1
SandaRaz <119030837+SandaRaz@users.noreply.github.com>
2023-04-04 09:05:46 +0300

    deleteAll

--- Controller/src/main/java/etu2079/framework/MethAnnotation.java
+++
@@ -1,9 +0,0 @@
-package etu2079.framework;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MethAnnotation {
-    public String name() default "";
-}
